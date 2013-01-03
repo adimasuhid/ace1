e19b2ad008e4182573bd6937a952ed3731e82344
Ace Dimasuhid <ace@victor.local>
2013-01-03 11:15:57 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ace1::Application.config.secret_token = '9370cc65ea7bdaf942ec3f63bd3bda31be0c01d1f4ae988d9378cb36d04aae67e082717a21bac8e4939ad35b962c0c60c472895f1a4ba946b9c5a43132e81f61'
